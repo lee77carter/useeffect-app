b97f9532c8bd42c7d8323af508d1be6438af0bf7
lee_c <gitleelee@gmail.com>
2023-12-17 15:48:20 -0800

    Clean-up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("friend");
+  const [personType, setPersonType] = useState("lovely");
+
+  const reactions = {
+    morning: "Awesome! ☀️",
+    afternoon: "Fantastic! 😎",
+    evening: "Amazing! 🌙"
+  };
+
+  // experiment with useEffect here!
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="form-element">
+        <label htmlFor="name" />
+        Name:
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="form-element">
+        <label htmlFor="personType" />
+        Select a person type:
+        <select
+          id="personType"
+          type="text"
+          value={personType}
+          onChange={(e) => setPersonType(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="lovely">Choose one!</option>
+          <option value="morning">morning</option>
+          <option value="afternoon">afternoon</option>
+          <option value="evening">evening</option>
+        </select>
+        <h1>Hello there, {name} !</h1>
+        <h3>
+          You are{" "}
+          {personType === "morning" || personType === "lovely" ? "a" : "an"}{" "}
+          {personType} person. {reactions[personType]}
+        </h3>
+        <p>
+          Update the name and then have a look at the console for the result of
+          the useEffect call. :)
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
